fb1d19ae74a50a465d251af3b056116a149e0535
Toni Esteves <toni.esteves@gmail.com>
2021-02-05 00:07:19 -0300

    add some prints

--- run_framework.py
+++ run_framework.py
@@ -8,18 +8,26 @@ N_NODES = [7, 4, 6]
 train_set, eval_set = dat.get_data_sets()
 
 sample = next(train_set())
-input_value_range =(-5, 5)
+input_value_range = (-5, 5)
 n_pixels = sample.shape[0] * sample.shape[1]
 
 n_nodes = [n_pixels] + N_NODES + [n_pixels]
 
 model = []
 
+# print("Hidden Layers")
+# print(N_NODES)
+#
+# print("Network Archteture")
+# print(n_nodes)
+
 for idx_layer in range(len(n_nodes) - 1):
-  model.append(Dense(
-    n_nodes[idx_layer],
-    n_nodes[idx_layer + 1],
-    tahn))
+    # print(" From " + str(n_nodes[idx_layer]) + " neurons to " + str(n_nodes[idx_layer + 1]) + " neurons.")
+    # print("--")
+    model.append(Dense(
+      n_nodes[idx_layer],
+      n_nodes[idx_layer + 1],
+      tahn))
 
 autoencoder = ANN(
   model          = model,
